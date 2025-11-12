3d21be97e7bbca7f881cd0763d38255e08db43fe
ivangm1 <ivangm1234@gmail.com>
2025-11-12 12:03:38 +0100

    mini fix

--- src/components/pedidoComponents/PedidoEtiquetas.jsx
+++ src/components/pedidoComponents/PedidoEtiquetas.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 function PedidoEtiquetas({ orderXml }) {
   const adhesivo = orderXml.actividad?.adhesivo;
